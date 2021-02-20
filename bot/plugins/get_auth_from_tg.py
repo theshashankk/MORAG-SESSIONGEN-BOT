062a7c5be9f2e6daeabbb95de6674cc1793011f1
r0ld3x <77254818+r0ld3x@users.noreply.github.com>
2021-02-20 13:54:37 +0530

    Update get_auth_from_tg.py

--- bot/plugins/get_auth_from_tg.py
+++ bot/plugins/get_auth_from_tg.py
@@ -81,8 +81,6 @@ async def recv_tg_code_message(_, message: Message):
         await saved_message_.reply_text(
             SESSION_GENERATED_USING,
             quote=True
-            session = client.session.save()
-           f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` ",
         )
         del AKTIFPERINTAH[message.chat.id]
         return False
